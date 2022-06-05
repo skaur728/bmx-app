907951c16c5799c38c4c31608a6e2cd4b449a9fd
vsarunhah <varunshah660@gmail.com>
2022-06-05 13:50:40 -0700

    added azure directory

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,7 @@
 import NextAuth from 'next-auth'
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import GoogleProvider from 'next-auth/providers/google'
+import AzureADProvider from 'next-auth/providers/azure-ad'
 import mongoConnect from '@/utils/store/mongoConnect'
 
 export default NextAuth({
@@ -9,6 +10,11 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.NEXTAUTH_CLIENT_ID || '',
       clientSecret: process.env.NEXTAUTH_CLIENT_SECRET || '',
+    }),
+    AzureADProvider({
+      clientId: process.env.AZURE_AD_CLIENT_ID || '',
+      clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
+      tenantId: process.env.AZURE_AD_TENANT_ID,
     }),
   ],
 })
