a1471123a31355da54278bc2728953c925609096
Charlie Sale <softwaresale01@gmail.com>
2022-09-10 13:05:46 -0400

    Add temporary navigation links to pages

--- src/pages/apply/index.tsx
+++ src/pages/apply/index.tsx
@@ -1,26 +1,37 @@
-import { Container, Typography, Box } from '@mui/material'
+import { Container, Typography, Box, Button } from '@mui/material'
 import { NextPage } from 'next'
+import { Router, useRouter } from 'next/router'
 
 interface Props {}
 
-const BMApplicationFormPage: NextPage<Props> = ({}) => (
-  <Container>
-    <Box
-      sx={{
-        my: 4,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant="h6">BoilerMake Application page</Typography>
-      <p>
-        This page holds the boilermake specific application questions. This page
-        will also be a form. DELETE THIS WHEN STUFF ACTUALLY GOES IN
-      </p>
-    </Box>
-  </Container>
-)
+const submitApplication = (router: Router, application: any | undefined) => {
+  console.log(application)
+  router.push('/user/profile')
+}
+
+const BMApplicationFormPage: NextPage<Props> = ({}) => {
+  const router = useRouter()
+
+  return (
+    <Container>
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6">BoilerMake Application page</Typography>
+        <p>
+          This page holds the boilermake specific application questions. This
+          page will also be a form. DELETE THIS WHEN STUFF ACTUALLY GOES IN
+        </p>
+      </Box>
+      <Button onClick={() => submitApplication(router, null)}>SUBMIT</Button>
+    </Container>
+  )
+}
 
 export default BMApplicationFormPage
--- src/pages/user/info.tsx
+++ src/pages/user/info.tsx
@@ -1,28 +1,40 @@
-import { Box, Container, Typography } from '@mui/material'
+import { Box, Button, Container, Typography } from '@mui/material'
 import { NextPage } from 'next'
+import { Router, useRouter } from 'next/router'
 
 interface Props {}
 
-const UserInfoFormPage: NextPage<Props> = ({}) => (
-  <Container maxWidth="lg">
-    <Box
-      sx={{
-        my: 4,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}
-    >
-      <Typography variant="h6">User Info form page</Typography>
-      <p>
-        This page has the user info form that should be filled out if the user
-        is new. This will be a form. After submitting this form, the user info
-        should be filled out for the current logged in user. The user should
-        then be sent to the BM application page.
-      </p>
-    </Box>
-  </Container>
-)
+const submitUserInfo = (router: Router, userObj: any | undefined) => {
+  console.log(userObj)
+  router.push('/apply')
+}
+
+const UserInfoFormPage: NextPage<Props> = ({}) => {
+  const router = useRouter()
+
+  return (
+    <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Typography variant="h6">User Info form page</Typography>
+        <p>
+          This page has the user info form that should be filled out if the user
+          is new. This will be a form. After submitting this form, the user info
+          should be filled out for the current logged in user. The user should
+          then be sent to the BM application page.
+        </p>
+      </Box>
+
+      <Button onClick={() => submitUserInfo(router, null)}>SUBMIT</Button>
+    </Container>
+  )
+}
 
 export default UserInfoFormPage
