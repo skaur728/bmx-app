b0c25029c473a4206c2a589c322227e8d996b767
Charlie Sale <softwaresale01@gmail.com>
2022-09-10 12:05:03 -0400

    Address feedback

--- src/pages/apply/index.tsx
+++ src/pages/apply/index.tsx
@@ -1,5 +1,4 @@
-import { Container, Typography } from '@mui/material'
-import { Box } from '@mui/system'
+import { Container, Typography, Box } from '@mui/material'
 import { NextPage } from 'next'
 
 interface Props {}
@@ -18,7 +17,7 @@ const BMApplicationFormPage: NextPage<Props> = ({}) => (
       <Typography variant="h6">BoilerMake Application page</Typography>
       <p>
         This page holds the boilermake specific application questions. This page
-        will also be a form.
+        will also be a form. DELETE THIS WHEN STUFF ACTUALLY GOES IN
       </p>
     </Box>
   </Container>
