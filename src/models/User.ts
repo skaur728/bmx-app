ce7ac833057c32f700633baee6b43cd04e8dcd9f
adityaavadhanam2002 <70170010+adityaavadhanam2002@users.noreply.github.com>
2022-09-14 17:26:37 -0700

    adi - User and Application Schema Defined (#25)
    * Changes to application and user.ts
    
    * changed application to create interface etc
    
    Co-authored-by: Aditya Avadhanam <aditya.avadhanam@gmail.com>

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,14 +2,31 @@ import { Schema, model, models } from 'mongoose'
 
 import type { User } from 'next-auth'
 
+enum Role {
+  'Applicant',
+  'Hacker',
+  'Exec',
+  'Sponsor',
+}
+
 export interface IUser extends User {
   firstName: string
   lastName: string
+  //email: string
+  //password: string //needs to be hashed
+  role: Role
+  //confirmation_code: string
+  profile_info: boolean
 }
 
 const UserSchema = new Schema<IUser>({
   firstName: String,
   lastName: String,
+  //email: String,
+  //password: String,
+  role: Role,
+  //confirmation_code: String,
+  profile_info: Boolean,
 })
 
 // must load plugin before model creation
