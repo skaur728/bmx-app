ea19902af2449c15325d0db6b96d2fcf7cb6da0b
Jacob Zhang <Jsbbvk@gmail.com>
2022-09-23 15:16:10 -0400

    Add application field to user schema (#28)
    added application field to user schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,5 +1,7 @@
 import { Schema, model, models } from 'mongoose'
 
+import type { IApplication } from './Application'
+import type { Model, Types } from 'mongoose'
 import type { User } from 'next-auth'
 
 enum Role {
@@ -9,27 +11,51 @@ enum Role {
   'Sponsor',
 }
 
+export type Applications = Record<string, Types.ObjectId>
+
 export interface IUser extends User {
   firstName: string
   lastName: string
-  //email: string
-  //password: string //needs to be hashed
   role: Role
-  //confirmation_code: string
   profile_info: boolean
+  applications: Applications
+  getApplicationByYear: (year: number) => Promise<IApplication>
+  addApplication: (year: number, id: Types.ObjectId) => void
 }
 
 const UserSchema = new Schema<IUser>({
   firstName: String,
   lastName: String,
-  //email: String,
-  //password: String,
-  role: Role,
-  //confirmation_code: String,
+  role: {
+    type: String,
+    enum: Role,
+  },
   profile_info: Boolean,
+  applications: {
+    default: {},
+    type: Map,
+    of: {
+      type: Schema.Types.ObjectId,
+      ref: 'Application',
+    },
+  },
 })
 
 // must load plugin before model creation
+// we can't use a global plugin since we can't guarantee that it'll be loaded before model compilation
 UserSchema.plugin(require('@/utils/store/leanObjectIdToString'))
 
-export default models.User || model<IUser>('User', UserSchema)
+UserSchema.methods.addApplication = function addApplication(
+  year: number,
+  id: Types.ObjectId
+) {
+  this.applications.set(String(year), id)
+}
+
+UserSchema.methods.getApplicationByYear = async function getApplicationByYear(
+  year: number
+) {
+  return (await this.populate('applications.$*')).applications.get(String(year))
+}
+
+export default (models.User as Model<IUser>) || model<IUser>('User', UserSchema)
