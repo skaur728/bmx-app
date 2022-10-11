11fa2494fd3b10016ef60deab5305b9a91357424
skaur <skaur@OREO>
2022-10-10 20:13:16 -0400

    added github provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
 import NextAuth from 'next-auth'
 import AzureADProvider from 'next-auth/providers/azure-ad'
 import GoogleProvider from 'next-auth/providers/google'
+import GitHubProvider from "next-auth/providers/github";
 
 import mongoConnect from '@/utils/store/mongoConnect'
 
@@ -17,6 +18,10 @@ export default NextAuth({
       clientSecret: process.env.AZURE_AD_CLIENT_SECRET || '',
       tenantId: process.env.AZURE_AD_TENANT_ID,
     }),
+      GitHubProvider({
+          clientId: process.env.GITHUB_ID,
+          clientSecret: process.env.GITHUB_SECRET
+      }),
   ],
   pages: {
     signIn: '/auth/signin',
