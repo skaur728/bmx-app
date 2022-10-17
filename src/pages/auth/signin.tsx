775e83c4cb0e80a07713f087842195b7e550e1dd
Savreen <70596495+skaur728@users.noreply.github.com>
2022-10-17 12:25:29 -0400

    sign in styling (#35)
    * basic sign in page styling
    
    * basic styling
    
    * prettier/eslint fixes
    
    Co-authored-by: skaur <skaur@OREO>
    Co-authored-by: Jacob Zhang <Jsbbvk@gmail.com>

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,3 +1,4 @@
+import { Button, Typography, styled } from '@mui/material'
 import { getProviders, signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
 
@@ -16,26 +17,80 @@ type Props = {
 
 const DEFAULT_CALLBACK = '/dashboard'
 
+const StyledButton = styled(Button)({
+  padding: '10px 40px',
+  borderRadius: '50px',
+  backgroundColor: '#893422',
+  color: '#ffe8c9',
+  boxShadow:
+    'rgb(0 0 0 / 20%) 0px 3px 3px -2px, rgb(0 0 0 / 14%) 0px 3px 4px 0px, rgb(0 0 0 / 12%) 0px 1px 8px 0px',
+  transition: 'transform 0.25s ease',
+  '&:hover': {
+    backgroundColor: '#792d1e',
+    transform: 'scale(1.04)',
+  },
+  '&:active': {
+    transform: 'scale(1)',
+  },
+})
 const SignIn = ({ providers }: Props) => {
   const { query } = useRouter()
 
   return (
-    <>
-      {Object.values(providers).map((provider) => (
-        <div key={provider.name}>
-          <button
-            onClick={() =>
-              signIn(provider.id, {
-                callbackUrl: (query?.redirect as string) || DEFAULT_CALLBACK,
-              })
-            }
-            type="button"
-          >
-            Sign in with {provider.name}
-          </button>
+    <div
+      style={{
+        height: '100%',
+        width: '100%',
+        position: 'fixed',
+        background: 'linear-gradient(#1c2634 60%,  #694028)',
+      }}
+    >
+      <div
+        style={{
+          backgroundImage: 'url(/images/main/stars.svg)',
+          backgroundRepeat: 'repeat',
+          backgroundSize: 'contain',
+          backgroundPosition: 'center',
+          height: '60vh',
+          width: '100%',
+          position: 'absolute',
+          top: 0,
+          left: '50%',
+          transform: 'translate(-50%)',
+        }}
+      >
+        <div
+          style={{
+            marginLeft: '555px',
+            marginTop: '200px',
+            background: 'white',
+            height: '40vh',
+            width: '50vh',
+            borderRadius: '20px',
+          }}
+        >
+          <Typography sx={{ fontSize: '50px', textAlign: 'center' }}>
+            Sign In
+          </Typography>
+          {Object.values(providers).map((provider) => (
+            <div key={provider.name} style={{ textAlign: 'center' }}>
+              <br />
+              <StyledButton
+                onClick={() =>
+                  signIn(provider.id, {
+                    callbackUrl:
+                      (query?.redirect as string) || DEFAULT_CALLBACK,
+                  })
+                }
+                type="button"
+              >
+                Sign in with {provider.name}
+              </StyledButton>
+            </div>
+          ))}
         </div>
-      ))}
-    </>
+      </div>
+    </div>
   )
 }
 
