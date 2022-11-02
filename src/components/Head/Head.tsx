08bd0f44cf0d963d009c03db691bab4b1decec0b
Jacob Zhang <jacobzhang132@gmail.com>
2022-11-02 11:42:32 -0400

    added card img to preview

--- src/components/Head/Head.tsx
+++ src/components/Head/Head.tsx
@@ -26,7 +26,7 @@ const HeadComponent = ({ title }: Props) => (
       property="og:description"
       content="BoilerMake is an annual, 36-hour hackathon at Purdue that brings over 500 hackers from across the country under one roof for one incredible weekend."
     />
-    <meta property="og:image" content="" />
+    <meta property="og:image" content="%PUBLIC_URL%/images/card.png" />
 
     <meta property="twitter:card" content="summary_large_image" />
     <meta property="twitter:url" content="https://www.boilermake.com/" />
@@ -35,7 +35,7 @@ const HeadComponent = ({ title }: Props) => (
       property="twitter:description"
       content="BoilerMake is an annual, 36-hour hackathon at Purdue that brings over 500 hackers from across the country under one roof for one incredible weekend."
     />
-    <meta property="twitter:image" content="" />
+    <meta property="twitter:image" content="%PUBLIC_URL%/images/card.png" />
   </Head>
 )
 
