8d6c27a2f98caa40aa087fe20540681756aab3f8
A-Wadhwani <wadhwani@purdue.edu>
2023-01-13 10:10:31 -0500

    Cleanup / Remove Message post

--- src/pages/api/slack/index.ts
+++ src/pages/api/slack/index.ts
@@ -3,30 +3,38 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import dbConnect from '@/utils/store/dbConnect'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
-import axios from 'axios'
 
-const SLACK_WEBHOOK: string = process.env.SLACK_WEBHOOK || ''
+const SLACK_TOKEN: string = process.env.SLACK_TOKEN || ''
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
     await dbConnect()
 
-    // Extract 'type' from req
-    const { type }: { type: string } = req.body
+    const { token, type }: { token: string; type: string } = req.body
+
+    if (token !== SLACK_TOKEN) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: ReasonPhrases.UNAUTHORIZED,
+      })
+    }
 
     if (type === 'url_verification') {
       // Slack is verifying the endpoint
       return res.status(StatusCodes.OK).json({ challenge: req.body.challenge })
     }
 
-    if (type == 'event_callback') {
-      // If a message is received, post it to Slack
+    if (type === 'event_callback') {
+      // If a message is received, store it on MongoDB
       const { event } = req.body
-      const { type, text }: { type: string; text: string } = event.type
-      await axios.post(SLACK_WEBHOOK, {
-        text: text,
+
+      if (event.type === 'message' && event.channel_type === 'channel') {
+        // TODO: Store the 'text' field
+        return res.status(StatusCodes.OK).json({ message: ReasonPhrases.OK })
+      }
+
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        message: ReasonPhrases.BAD_REQUEST,
       })
-      return res.status(StatusCodes.OK).json({ message: ReasonPhrases.OK })
     }
 
     return res
