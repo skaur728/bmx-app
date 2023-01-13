4ede28daab0cf3737b1009e9beb7d5317328608d
A-Wadhwani <wadhwani@purdue.edu>
2023-01-13 09:48:00 -0500

    Send Message to Webhook

--- src/pages/api/slack/index.ts
+++ src/pages/api/slack/index.ts
@@ -1,11 +1,11 @@
-import to from 'await-to-js'
 import { ReasonPhrases, StatusCodes } from 'http-status-codes'
-import { getToken } from 'next-auth/jwt'
 
-import { updateApplicationByYear } from '@/controllers/application'
 import dbConnect from '@/utils/store/dbConnect'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
+import axios from 'axios'
+
+const SLACK_WEBHOOK: string = process.env.SLACK_WEBHOOK || ''
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === 'POST') {
@@ -19,7 +19,19 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
       return res.status(StatusCodes.OK).json({ challenge: req.body.challenge })
     }
 
-    return res.status(StatusCodes.OK).json({ message: 'OK' })
+    if (type == 'event_callback') {
+      // If a message is received, post it to Slack
+      const { event } = req.body
+      const { type, text }: { type: string; text: string } = event.type
+      await axios.post(SLACK_WEBHOOK, {
+        text: text,
+      })
+      return res.status(StatusCodes.OK).json({ message: ReasonPhrases.OK })
+    }
+
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .json({ message: ReasonPhrases.BAD_REQUEST })
   }
   return res
     .status(StatusCodes.METHOD_NOT_ALLOWED)
