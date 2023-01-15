28aba242c001c510fe698bf9571b78167b51418a
A-Wadhwani <wadhwani@purdue.edu>
2023-01-15 15:04:07 -0500

    Store announcements in MongoDB and test page

---
+++ src/controllers/announcement.ts
@@ -0,0 +1,30 @@
+import to from 'await-to-js'
+
+import Announcement, { IAnnouncementDocument } from '@/models/Announcement'
+
+export const createAnnouncement = async (message: string) => {
+  if (!message) {
+    throw new Error('Message is required')
+  }
+  const [creationError, app] = await to<IAnnouncementDocument, Error>(
+    Announcement.create({ message })
+  )
+  if (creationError || !app) {
+    throw !app
+      ? new Error('Announcement could not be created')
+      : new Error(creationError)
+  }
+}
+
+export const getAnnouncements = async () => {
+  const [findError, announcements] = await to(
+    Announcement.find({}).sort('createdAt').exec()
+  )
+
+  if (findError || !announcements) {
+    throw !announcements ? new Error('No user found') : findError!
+  }
+
+  // return promise so function caller handles errors
+  return announcements
+}
--- src/pages/api/slack/index.ts
+++ src/pages/api/slack/index.ts
@@ -3,6 +3,11 @@ import { ReasonPhrases, StatusCodes } from 'http-status-codes'
 import dbConnect from '@/utils/store/dbConnect'
 
 import type { NextApiRequest, NextApiResponse } from 'next'
+import {
+  createAnnouncement,
+  getAnnouncements,
+} from '@/controllers/announcement'
+import to from 'await-to-js'
 
 const SLACK_TOKEN: string = process.env.SLACK_TOKEN || ''
 
@@ -24,23 +29,39 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     }
 
     if (type === 'event_callback') {
-      // If a message is received, store it on MongoDB
       const { event } = req.body
 
       if (event.type === 'message' && event.channel_type === 'channel') {
-        // TODO: Store the 'text' field
+        // If a message is received, store it on MongoDB
+        // TODO: Store the 'text' field and timestamp
+        const [error, _] = await to(createAnnouncement(event.text))
+        if (error) {
+          return res
+            .status(StatusCodes.BAD_REQUEST)
+            .send({ message: error.message })
+        }
         return res.status(StatusCodes.OK).json({ message: ReasonPhrases.OK })
       }
-
-      return res.status(StatusCodes.BAD_REQUEST).json({
-        message: ReasonPhrases.BAD_REQUEST,
-      })
     }
 
     return res
       .status(StatusCodes.BAD_REQUEST)
       .json({ message: ReasonPhrases.BAD_REQUEST })
   }
+
+  if (req.method === 'GET') {
+    await dbConnect()
+
+    const [error, announcements] = await to(getAnnouncements())
+    if (error) {
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .send({ message: ReasonPhrases.INTERNAL_SERVER_ERROR })
+    }
+
+    return res.send({ announcements })
+  }
+
   return res
     .status(StatusCodes.METHOD_NOT_ALLOWED)
     .send({ message: ReasonPhrases.METHOD_NOT_ALLOWED })
